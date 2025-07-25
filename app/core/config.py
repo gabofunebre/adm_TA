44b5120f300d64424b0b26224f1992226a6757ea
gabofunebre <gabofunebre@gmail.com>
2025-07-25 19:19:01 -0300

    intento

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,14 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET_KEY: str
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_port: int
+    admin_username: str
+    admin_password: str
+    google_client_id: str
+    google_client_secret: str
 
     JWT_ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
