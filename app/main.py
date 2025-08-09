bedf5db408a2d27070ec633661c12835f9bc8fde
gabofunebre <136429132+gabofunebre@users.noreply.github.com>
2025-08-09 02:17:55 +0100

    Refactor project to adm_TA with login and registration only

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from app.api.routers import auth, users, accounts, movements, dashboard, registration, orders
+from app.api.routers import auth, registration
 from app.database import Base, engine, SessionLocal
 from app.services.logging_middleware import LoggingMiddleware
-from pathlib import Path
 import app.models
 from app.core.security import get_password_hash
 from app.core.config import get_settings
@@ -28,20 +26,9 @@ with SessionLocal() as db:
         db.add(admin_user)
         db.commit()
 
-app = FastAPI(title="Movimientos de Dinero")
+app = FastAPI(title="adm_TA")
 
 app.add_middleware(LoggingMiddleware)
 
-
 app.include_router(auth.router)
-app.include_router(users.router)
-app.include_router(accounts.router)
-app.include_router(movements.router)
-app.include_router(dashboard.router)
 app.include_router(registration.router)
-app.include_router(orders.router)
-app.mount(
-    "/",
-    StaticFiles(directory=Path(__file__).parent.parent / "frontend" / "dist", html=True),
-    name="frontend",
-)
